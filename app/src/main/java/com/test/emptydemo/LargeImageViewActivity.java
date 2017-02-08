5b455b3700621f3d487f32d85dcd1dff74dd597b
zxzxzxygithub <924106037@qq.com>
2017-02-08 23:20:13 +0800

    add:BitmapRegionDecoderDemo

---
+++ app/src/main/java/com/test/emptydemo/LargeImageViewActivity.java
@@ -0,0 +1,48 @@
+package com.test.emptydemo;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LargeImageViewActivity extends Activity {
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitmaregindecoder);
+
+        mImageView = (ImageView) findViewById(R.id.id_imageview);
+        try {
+            InputStream inputStream = getAssets().open("tianyan.jpg");
+
+            //获得图片的宽、高
+            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+            tmpOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(inputStream, null, tmpOptions);
+            int width = tmpOptions.outWidth;
+            int height = tmpOptions.outHeight;
+
+            //设置显示图片的中心区域
+            BitmapRegionDecoder bitmapRegionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.RGB_565;
+            Bitmap bitmap = bitmapRegionDecoder.decodeRegion(new Rect(width / 2 - 100, height / 2 - 100, width / 2 + 100, height / 2 + 100), options);
+            mImageView.setImageBitmap(bitmap);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
