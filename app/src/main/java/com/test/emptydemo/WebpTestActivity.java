cae086ee0be797ae7dd9115ae2d48cd855ffdb47
郑永祥 <924106037@qq.com>
2017-03-02 14:07:43 +0800

    add:webp可以被imageview展示

---
+++ app/src/main/java/com/test/emptydemo/WebpTestActivity.java
@@ -0,0 +1,13 @@
+package com.test.emptydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WebpTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webp_test);
+    }
+}
