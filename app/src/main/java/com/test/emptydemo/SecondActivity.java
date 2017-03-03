e2558448c2d7d5f1785bc0cd1ad88b9b39f00af3
郑永祥 <924106037@qq.com>
2017-03-03 20:01:44 +0800

    add:在浏览器中打开你的应用

--- app/src/main/java/com/test/emptydemo/SecondActivity.java
+++ app/src/main/java/com/test/emptydemo/SecondActivity.java
@@ -2,8 +2,10 @@ package com.test.emptydemo;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,26 +22,25 @@ public class SecondActivity extends Activity implements View.OnClickListener {
 
     @BindView(R.id.tv)
     TextView tv;
+    @BindView(R.id.iv)
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         ButterKnife.bind(this);
-        tv.setText("try to click me");
+        tv.setText("try to write http://siliao.com/app/  or  http://siliao.com/detail to qq\n 在qq选择用浏览器打开你的这个链接就会弹出对话框调用你的应用");
     }
 
     @OnClick(R.id.tv)
     @Override
     public void onClick(View v) {
-
         int id = v.getId();
         switch (id) {
             case R.id.tv:
                 Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
                 break;
         }
-
-
     }
 }
