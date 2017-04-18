520f17ca116ada711c0db4c25b27028bd60e0e2d
郑永祥 <924106037@qq.com>
2017-04-18 15:55:36 +0800

    add：1.添加注释，  2.增加studio版本号

--- app/src/main/java/com/test/emptydemo/xposed/Main.java
+++ app/src/main/java/com/test/emptydemo/xposed/Main.java
@@ -7,12 +7,16 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-
+/**
+ * @author zhengyx
+ * @description xpose模块处理入口
+ * @date 2017/4/18
+ */
 public class Main implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-//非本应用不过滤
+//        hook telephonymanager的getDeviceId方法，修改返回值为11111111111
         XposedHelpers.findAndHookMethod(TelephonyManager.class, "getDeviceId", new Object[]{new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
